0d88d157a3910f8c0ef64198c0073d7497865fe6
dlcjfgmlnasa <dlcjfgmlnasa@naver.com>
2019-08-22 10:31:14 +0900

    GIN 부동산 예측 프로젝트 Setting 파일

---
+++ settings.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+# env path
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path=env_path)
+
+# db connections
+cnx = mysql.connector.MySQLConnection(
+    user=os.getenv('db_user'),
+    password=os.getenv('db_password'),
+    host=os.getenv('db_host'),
+    port=os.getenv('db_port'),
+    database=os.getenv('db_name')
+)
+db_cursor = cnx.cursor()
